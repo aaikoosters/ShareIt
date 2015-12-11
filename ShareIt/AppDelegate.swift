b4593cfe94795e9283957d0ff040118043f2feed
AaikOosters <aaik.oosters@gmail.com>
2015-12-11 09:46:59 +0100

    FrameWorks Added
    Connectie gemaakt met parse

--- ShareIt/AppDelegate.swift
+++ ShareIt/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios/guide#local-datastore
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("gj8RJE5QGckS8NL7EO1wuXzLsKKcjtFJRSDd6sMi",
+            clientKey: "su74KYse62eWnJPu2MegTuH3psp5SbHtHGEIyXLu")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
