3b27839f92473a3657fe2e2e0c699c7fc7c848f5
bgaldeij <student@henks-mac-mini.wireless.hhs.nl>
2016-01-19 13:33:45 +0100

    fixed while 1 == 1

--- ShareIt/EventListTableViewController.swift
+++ ShareIt/EventListTableViewController.swift
@@ -74,11 +74,6 @@ class EventListTableViewController: UITableViewController, UISearchBarDelegate {
             users in
             dispatch_async(dispatch_get_main_queue(),
                 {
-
-                    while 1 == 1
-                    {
-                        
-                    }
                 self.refreshControl?.endRefreshing()
                 self.tableView.reloadData()
             })
